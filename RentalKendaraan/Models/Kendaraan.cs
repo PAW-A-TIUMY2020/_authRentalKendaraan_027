568cee6ad465ee7423eab8149e04247c92bd2ee2
Auliyauswatunkhasanah <auliyauswatunn@gmail.com>
2020-12-06 00:45:06 +0700

    Add project files.

---
+++ RentalKendaraan/Models/Kendaraan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_20180140027.Models
+{
+    public partial class Kendaraan
+    {
+        public Kendaraan()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Nama kendaraan tidak boleh kosong")]
+        public string NamaKendaraan { get; set; }
+
+        [Required(ErrorMessage = "No polisi  wajib di isi")]
+        public string NoPolisi { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "stnk hanya boleh di isi dengan angka")]
+        public string NoStnk { get; set; }
+
+        public string Ketersediaan { get; set; }
+
+        [Required(ErrorMessage = "ketersediaan  wajib di isi")]
+        public int? IdJenisKendaraan { get; set; }
+
+        public JenisKendaraan IdJenisKendaraanNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
---
+++ RentalKendaraan/Models/Pengembalian.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_20180140027.Models
+{
+    public partial class Pengembalian
+    {
+        public int IdPengembalian { get; set; }
+
+        [Required(ErrorMessage = "Tgl peminjaman wajib diisi")]
+        public DateTime? TglPengembalian { get; set; }
+        public int? Denda { get; set; }
+        public int? IdPeminjaman { get; set; }
+
+        [Required(ErrorMessage = "Denda wajib diisi")]
+        public int? IdKondisi { get; set; }
+
+        public KondisiKendaraan IdKondisiNavigation { get; set; }
+        public Peminjaman IdPeminjamanNavigation { get; set; }
+    }
+}
